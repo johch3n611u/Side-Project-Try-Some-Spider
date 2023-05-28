abf71c1ca8866a374b6db62f77fa80b81a688cd7
DESKTOP-9SELDLU\user <sjmjgd1165@gmail.com>
2023-05-28 17:29:49 +0800

    stock

--- GAS-Stock.js
+++ GAS-Stock.js
@@ -1,12 +1,11 @@
 function myFunction() {
     let sheet = initSheet();
-    // sendToLine(encodeURIComponent(getFormData(sheet)));
+    sendToLine(encodeURIComponent(getFormData(sheet)));
     // sendToDiscord(getFormData(sheet));
-    maxAndMin30d(sheet);
 }
 
 function initSheet() {
-    let SpreadSheet = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1Pw1hj-LDVy4Yyqa5UbexPfRUmOr3mp84bL_UAdG8XU4/edit#gid=1381773314');
+    let SpreadSheet = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1WJM0l_GL-tVPPPJkoUf4AA7swb3PsZ16KHtfthLf_Z4/edit#gid=0');
     return SpreadSheet.getSheetByName('stock');
 }
 
@@ -14,11 +13,14 @@ function getFormData(sheet) {
 
     let secondLastRow = sheet.getRange(sheet.getLastRow() - 1, 1, 1, sheet.getLastColumn()).getValues()[0];
     let lastRow = sheet.getRange(sheet.getLastRow(), 1, 1, sheet.getLastColumn()).getValues()[0];
+    var range = sheet.getDataRange();
+    var values = range.getValues();
+    var header = values.shift();
 
     // date,tw_stock,tw_0050,SP500,VOO,QQQ,BID
     // [2023/0526, 16505.05, 125.95, 4151.28, 380.92, 339.72, 7.0]
 
-    let msg = '';
+    let msg = `${lastRow[0]}，以下為 ${values.length} 天均線\n`;
 
     // msg += `${lastRow[0]}\n台股大盤現值為${lastRow[1]}相較上一次紀錄${secondLastRow[1]}，漲幅為${percentageIncrease(lastRow, secondLastRow, 1)}`;
     // msg += `\n0050現值為${lastRow[2]}相較上一次紀錄${secondLastRow[2]}，漲幅為${percentageIncrease(lastRow, secondLastRow, 2)}`;
@@ -27,10 +29,10 @@ function getFormData(sheet) {
     // msg += `\nVOO現值為${lastRow[4]}相較上一次紀錄${secondLastRow[4]}，漲幅為${percentageIncrease(lastRow, secondLastRow, 4)}`;
     // msg += `\nQQQ現值為${lastRow[5]}相較上一次紀錄${secondLastRow[5]}，漲幅為${percentageIncrease(lastRow, secondLastRow, 5)}`;
 
-    msg += `\n台指, Today：${lastRow[1]}，漲幅${percentageIncrease(lastRow, secondLastRow, 1)}, 30d Max：1xxxx, Min：1xxxxx.`;
-    msg += `\n0050, Today：${lastRow[2]}，漲幅${percentageIncrease(lastRow, secondLastRow, 2)}, 30d Max：1xxxx, Min：1xxxxx.`;
-    msg += `\nS&P500, Today${lastRow[3]}，漲幅${percentageIncrease(lastRow, secondLastRow, 3)}, 30d Max：1xxxx, Min：1xxxxx.`;
-    msg += `\nQQQ, Today${lastRow[5]}，漲幅${percentageIncrease(lastRow, secondLastRow, 5)}, 30d Max：1xxxx, Min：1xxxxx.`;
+    msg += `\n台指：${Math.round(lastRow[1])}，漲幅${percentageIncrease(lastRow, secondLastRow, 1)}, ${maxAndMin(sheet, 1)}`;
+    msg += `\n0050：${Math.round(lastRow[2])}，漲幅${percentageIncrease(lastRow, secondLastRow, 2)}, ${maxAndMin(sheet, 2)}`;
+    msg += `\nS&P500：${Math.round(lastRow[3])}，漲幅${percentageIncrease(lastRow, secondLastRow, 3)}, ${maxAndMin(sheet, 3)}`;
+    msg += `\nQQQ：${Math.round(lastRow[5])}，漲幅${percentageIncrease(lastRow, secondLastRow, 5)}, ${maxAndMin(sheet, 5)}`;
 
     return msg;
 }
@@ -94,11 +96,44 @@ function businessIndicatorsDataBase(lastRow, cal) {
     return `${msg}`
 }
 
-function maxAndMin30d(sheet)
-{
+function maxAndMin(sheet, cal) {
     var range = sheet.getDataRange();
     var values = range.getValues();
-    var header = values[0];
-    
-    console.log(values);
+    var header = values.shift();
+
+    let row = [];
+    values.forEach((val) => {
+        row.push(val[cal]);
+    });
+
+    let sum = row.reduce(function (acc, curr) {
+        return acc + curr;
+    }, 0);
+
+    let average = Math.round(sum / values.length);
+    let msg = '';
+    let drop = row[values.length - 1] - average;
+    let suggestion = '';
+
+    if (row[values.length - 1] > average) {
+        msg = `大於均線　${average}　(+${Math.round(drop)}) `;
+        suggestion = '不建議購買';
+    } else {
+        msg = `小於均線　${average}　(-${Math.round(drop)}) `;
+        suggestion = '建議購買';
+    }
+
+    let averageDrop = Math.round(drop / average);
+
+    if (averageDrop > 20) {
+        msg += '超級無敵';
+    } else if (averageDrop > 15) {
+        msg += '極度';
+    } else if (averageDrop > 10) {
+        msg += '非常';
+    }
+
+    msg = msg + suggestion;
+
+    return msg;
 }
